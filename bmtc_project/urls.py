ea3ea30690aaeb952f39cf8922f41b21f18b2d7e
Aman Tanwar <amantanwar1994@gmail.com>
2017-03-15 18:20:15 +0530

    Basic Setup

---
+++ bmtc_project/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from bmtc_app import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'bmtc_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/',views.home,name='home'),
+]
